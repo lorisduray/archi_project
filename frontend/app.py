4da0c006f0f13d36ea90b492d8a4a5ecb1aeb93b
loris duray <lorisduray@gmail.com>
2024-08-20 12:39:48 +0200

    streamlit app, working upload image

--- frontend/app.py
+++ frontend/app.py
@@ -2,47 +2,41 @@ import streamlit as st
 from PIL import Image
 import requests
 import io
-import openai
+# import openai
 
-openai.api_key ='key_here'
+# openai.api_key ='key_here'
 
-# st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Architectural Styles Recognition App")
-st.write("Upload or capture an image of an architectural style, and the app will identify the style and provide detailed information.")
+st.write("Upload an image of an architectural style, and the app will identify the style and provide detailed information.")
 
-# Image upload or capture section
-st.subheader("Step 1: Upload or Capture an Image")
+st.subheader("Step 1: Upload an Image")
 
-# Upload image
 file_up = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-# Capture image
-capture = st.camera_input("Or, take a photo")
+# Loris here this is another options to take a photo, because by default it is better to keep it as button from GUI perspective
+# with st.expander("Or, capture an image using your camera"):
+#     capture = st.camera_input("Take a photo")
 
-if file_up is not None or capture is not None:
-    # Use the uploaded image or captured image
-    image = Image.open(file_up) if file_up else Image.open(capture)
+if file_up : # or capture
+    image = Image.open(file_up)
+    # if file_up else Image.open(capture)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("Just a second...")
+    # st.write("Just a second...") # I have commented this because it does not make scene this message when we get prediction is showing that it is loading
 
-    # Convert the image to bytes
     img_byte_arr = io.BytesIO()
     image.save(img_byte_arr, format='JPEG')
     img_byte_arr = img_byte_arr.getvalue()
 
-    # Send the image to the API
-    api_url = "http://localhost:8000/upload_image"  # Replace with google cloud?
-    response = requests.post(api_url, files={"file": img_byte_arr})
+    api_url = "http://localhost:8000/upload_image" # Replace with google cloud?
+    response = requests.post(api_url, files={"img": img_byte_arr})
 
     if response.status_code == 200:
         labels = response.json()
 
-        # Display the recognized style and probability
-        st.subheader("Step 2: Recognized Architectural Style")
+        # st.subheader("Step 2: Recognized Architectural Style")
         st.write(f"**Style Recognized:** {labels['architectural_style']}")
         st.write(f"**Probability:** {labels['probability']*100:.2f}%")
 
-        # # Fetch more information using OpenAI GPT
         # st.subheader("Step 3: Style Information")
         # style_info = openai.Completion.create(
         #     model="text-davinci-003",
@@ -51,6 +45,7 @@ if file_up is not None or capture is not None:
         # )
 
         # st.write(style_info.choices[0].text.strip())
+
 
     else:
         st.error("There was an error processing the image. Please try again.")
